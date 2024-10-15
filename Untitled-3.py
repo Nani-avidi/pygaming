7c4ff8de9f17d4a9dddaa98ea93c52edacffc6df
Nani-avidi <raghavendraavidi2003@gmail.com>
2024-10-15 10:40:56 +0530

    This is aestroids Game based on Python

---
+++ Untitled-3.py
@@ -0,0 +1,24 @@
+
+'''
+Integer --> 1,2,3,4,
+Float --> 1.2,2.5
+String --> 'Sequence of characters '' or ""
+Boolean --> 0 or 1, True or False
+print(type(variablename))
+'''
+'''
+TYPE CONVERSION
+------------------
+int(a)
+float(a)
+str()
+'''
+#a = 10 --> str
+a = 1.2 
+b="12"
+#print(type(a))
+#print(type(b))
+a=int(a)
+b=int(b)
+print(type(a))
+print(type(b))
